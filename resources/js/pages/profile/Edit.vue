e7d2493f9bf0e20e72c93e90d230a909713492d3
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2025-01-06 05:48:37 +0700

    add logout button on profile page for mobile

--- resources/js/pages/profile/Edit.vue
+++ resources/js/pages/profile/Edit.vue
@@ -8,10 +8,13 @@ const title = 'Profil Saya';
 <template>
   <i-head :title="title" />
   <authenticated-layout>
-    <template #left-button>
+    <template #left-button v-if="$q.screen.lt.md">
       <q-btn icon="arrow_back" dense flat @click="router.get(route('memorization.index'))"/>
     </template>
     <template #title>{{ title }}</template>
+    <template #right-button v-if="$q.screen.lt.md">
+      <q-btn icon="logout" label="Logout" dense  @click="router.get(route('logout'))"/>
+    </template>
     <div class="row justify-center">
       <div class="col col-lg-6 q-py-md q-px-sm q-gutter-md">
         <UpdateProfileInformationForm />
